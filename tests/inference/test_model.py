f355bdd537717431b64d2c48ba82bff32e4e2fdb
Giacomo Lanciano <giacomo.lanciano@sns.it>
2021-04-01 12:19:25 +0200

    Refactor tests suite

--- tests/inference/test_model.py
+++ tests/inference/test_model.py
@@ -1,7 +1,6 @@
 import logging
 import pathlib
 
-from monasca_predictor.common import util
 from monasca_predictor.inference.model import Model
 
 log = logging.getLogger(__name__)
@@ -12,9 +11,8 @@ def test_model():
     model_dump = (tests_dir / "model.h5").absolute()
     scaler_dump = (tests_dir / "scaler.joblib").absolute()
 
-    log.debug(tests_dir)
-    log.debug(model_dump)
-    log.debug(scaler_dump)
+    log.debug("Using model dump at '%s'.", model_dump)
+    log.debug("Using scaler dump at '%s'.", scaler_dump)
 
     model = Model()
     model.load(
@@ -23,12 +21,10 @@ def test_model():
     )
 
     x = [0.0] * 24
-
     y = model.predict(x)
 
-    log.debug("y (%s):\n%s", type(y), str(y))
+    log.debug("y (%s): %s", type(y), str(y))
 
 
 if __name__ == "__main__":
-    util.initialize_logging("predictor")
     test_model()
